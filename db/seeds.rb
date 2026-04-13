5da27ee0cba282ff4d134a690f641855c7b55fd4
Raafay59 <raafay.ah@gmail.com>
2026-04-13 18:34:48 -0500

    new test user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-admin_emails = %w[raafay@tamu.edu zzh021015@tamu.edu ernest01@tamu.edu]
+admin_emails = %w[raafay@tamu.edu zzh021015@tamu.edu ernest01@tamu.edu t47735295@gmail.com]
 
 admin_emails.each do |email|
   User.upsert({ email: email, role: "admin" }, unique_by: :email)
