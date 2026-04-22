c4b392353b82b5cdac47baf1844f3ddaaf59e842
WilliamT-byte <williamt2023@tamu.edu>
2026-04-21 21:06:19 -0500

    Fix indentation in HttpUrlValidation concern

--- app/models/concerns/http_url_validation.rb
+++ app/models/concerns/http_url_validation.rb
@@ -6,17 +6,17 @@ module HttpUrlValidation
 
      private
 
-       def validate_http_or_https_url(attribute)
-            value = public_send(attribute)
-            return if value.blank?
+          def validate_http_or_https_url(attribute)
+               value = public_send(attribute)
+               return if value.blank?
 
-            begin
-                 uri = URI.parse(value)
-            rescue URI::InvalidURIError
-                 errors.add(attribute, "must be a valid URL")
-                 return
-            end
+               begin
+                    uri = URI.parse(value)
+               rescue URI::InvalidURIError
+                    errors.add(attribute, "must be a valid URL")
+                    return
+               end
 
-            errors.add(attribute, "must be a valid HTTP or HTTPS URL") unless uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS)
-       end
+               errors.add(attribute, "must be a valid HTTP or HTTPS URL") unless uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS)
+          end
 end
